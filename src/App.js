f724775798a29295ba65ce45726973f28518920a
washogo <aiz4338150@gmail.com>
2021-12-11 20:00:20 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = props => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-  const reset = () => setCount(0)
-  const multiply = () => setCount(prevCount => prevCount * 2)
-  // const triple = () => {
-  //   if (count%3 === 0){
-  //     setCount(count / 3)
-  //   }
-  // }
-  // const triple = () => setCount(prevCount => {
-  //   if(prevCount % 3 === 0){
-  //     return prevCount / 3
-  //   }else{
-  //     return prevCount
-  //   }
-  // })
-  const triple = () => setCount(prevCount => prevCount % 3 === 0 ? prevCount / 3 : prevCount)
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
 
   return (
     <>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button onClick={multiply}>x2</button>
-        <button onClick={triple}>３の倍数の時だけ３で割る</button>
-      </div>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </>
   )
 }
+
+App.defaultProps = {
+    name: '',
+    price: 1000
+  }
 
 export default App
